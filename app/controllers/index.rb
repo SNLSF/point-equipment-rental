f853c096516df8807567a9f93584ad2cd4af1aa6
SNLSF <stephanielopez.sf@gmail.com>
2015-12-26 17:05:19 -0800

    responsive nav with hamburger

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,10 +1,3 @@
 get '/' do
 	erb :'equipment/index'
 end
-
-# HELPER METHODS
-
-# boolean to trigger active page css
-def active_page?(path='')
-  request.path_info == '/' + path
-end
--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
 
-	console.log('nav a[href^="/' + location.pathname.split("/")[1] + '"]');
-	 $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+  if ((location.pathname.split("/")[1]) !== ""){
+    $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+  }
+
+  $("#nav__hamburger").click(function(){
+  	if($('.nav__links a').hasClass('retract')) {
+  		$('.nav__links a').removeClass('retract');
+    	$('#js-toggle').slideUp();
+  	} else {
+  		$('.nav__links a').addClass('retract');
+    	$('#js-toggle').slideDown();
+  	}
+  });
+
 
 });
