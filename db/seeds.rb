b1f29c989ea7ef9c0319ba1e147b2e997ab20e1a
SNLSF <stephanielopez.sf@gmail.com>
2016-05-12 08:40:47 -0700

    basic formatting for desktop only

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,7 @@ equipment = [
 		monthly_price: "$900.00", 
 		spec_url: "http://www.ritchiespecs.com/specification?type=&category=Boom+Lift&make=Genie&model=Z60%2F34&modelid=108210", 
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Aenean blandit eu diam eu dictum. In a ipsum quis felis vestibulum hendrerit. 
-		Nulla eget tincidunt urna, ac aliquet ante. Nullam et bibendum libero. 
-		Aenean vitae convallis felis. Curabitur nibh tortor, hendrerit at rutrum ac, 
-		molestie et nisl. Mauris consectetur mi a dolor porttitor, eu tempus velit 
-		malesuada. Duis iaculis sit amet mi sit amet hendrerit."
+		Aenean blandit eu diam eu dictum."
 	},
 	{
 		name: "Mustang Skid Steer 2044", 
@@ -21,11 +17,17 @@ equipment = [
 		monthly_price: "$900.00", 
 		spec_url: "http://www.ritchiespecs.com/specification?category=Skid%20Steer%20Loader&make=MUSTANG&model=2044&modelid=92147", 
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Aenean blandit eu diam eu dictum. In a ipsum quis felis vestibulum hendrerit. 
-		Nulla eget tincidunt urna, ac aliquet ante. Nullam et bibendum libero. 
-		Aenean vitae convallis felis. Curabitur nibh tortor, hendrerit at rutrum ac, 
-		molestie et nisl. Mauris consectetur mi a dolor porttitor, eu tempus velit 
-		malesuada. Duis iaculis sit amet mi sit amet hendrerit."
+		Aenean blandit eu diam eu dictum."
+	},
+	{
+		name: "Mustang MTL20 Track Loader", 
+		category: "Skidsteer",
+		daily_price: "$100.00", 
+		weekly_price: "$300.00", 
+		monthly_price: "$900.00", 
+		spec_url: "http://www.ritchiespecs.com/specification?category=Skid%20Steer%20Loader&make=MUSTANG&model=2044&modelid=92147", 
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+		Aenean blandit eu diam eu dictum."
 	},
 	{
 		name: "JCB 508-40 Telescopic Forklift", 
@@ -35,11 +37,7 @@ equipment = [
 		monthly_price: "$900.00", 
 		spec_url: "$100.00", 
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Aenean blandit eu diam eu dictum. In a ipsum quis felis vestibulum hendrerit. 
-		Nulla eget tincidunt urna, ac aliquet ante. Nullam et bibendum libero. 
-		Aenean vitae convallis felis. Curabitur nibh tortor, hendrerit at rutrum ac, 
-		molestie et nisl. Mauris consectetur mi a dolor porttitor, eu tempus velit 
-		malesuada. Duis iaculis sit amet mi sit amet hendrerit."
+		Aenean blandit eu diam eu dictum."
 	},
 	{
 		name: "JCB 506-b Telescopic Forklift", 
@@ -49,11 +47,7 @@ equipment = [
 		monthly_price: "$900.00", 
 		spec_url: "http://www.ritchiespecs.com/specification?type=&category=Telescopic+Forklift&make=JCB&model=508C&modelid=94220", 
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Aenean blandit eu diam eu dictum. In a ipsum quis felis vestibulum hendrerit. 
-		Nulla eget tincidunt urna, ac aliquet ante. Nullam et bibendum libero. 
-		Aenean vitae convallis felis. Curabitur nibh tortor, hendrerit at rutrum ac, 
-		molestie et nisl. Mauris consectetur mi a dolor porttitor, eu tempus velit 
-		malesuada. Duis iaculis sit amet mi sit amet hendrerit."
+		Aenean blandit eu diam eu dictum."
 	},
 	{
 		name: "Scissor Lift", 
@@ -63,11 +57,7 @@ equipment = [
 		monthly_price: "$900.00", 
 		spec_url: "#", 
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Aenean blandit eu diam eu dictum. In a ipsum quis felis vestibulum hendrerit. 
-		Nulla eget tincidunt urna, ac aliquet ante. Nullam et bibendum libero. 
-		Aenean vitae convallis felis. Curabitur nibh tortor, hendrerit at rutrum ac, 
-		molestie et nisl. Mauris consectetur mi a dolor porttitor, eu tempus velit 
-		malesuada. Duis iaculis sit amet mi sit amet hendrerit."
+		Aenean blandit eu diam eu dictum."
 	},	
 	{
 		name: "GS™-1530 & GS™-1930", 
@@ -77,11 +67,7 @@ equipment = [
 		monthly_price: "$900.00", 
 		spec_url: "http://www.ritchiespecs.com/specification?type=&category=Scissor+Lift&make=Genie&model=GS1530&modelid=108584", 
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Aenean blandit eu diam eu dictum. In a ipsum quis felis vestibulum hendrerit. 
-		Nulla eget tincidunt urna, ac aliquet ante. Nullam et bibendum libero. 
-		Aenean vitae convallis felis. Curabitur nibh tortor, hendrerit at rutrum ac, 
-		molestie et nisl. Mauris consectetur mi a dolor porttitor, eu tempus velit 
-		malesuada. Duis iaculis sit amet mi sit amet hendrerit."
+		Aenean blandit eu diam eu dictum."
 	},	
 	{
 		name: "GS™-2032 & GS™-2632", 
@@ -91,11 +77,27 @@ equipment = [
 		monthly_price: "$900.00", 
 		spec_url: "http://www.ritchiespecs.com/specification?type=&category=Scissor+Lift&make=Genie&model=GS2032&modelid=108588", 
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Aenean blandit eu diam eu dictum. In a ipsum quis felis vestibulum hendrerit. 
-		Nulla eget tincidunt urna, ac aliquet ante. Nullam et bibendum libero. 
-		Aenean vitae convallis felis. Curabitur nibh tortor, hendrerit at rutrum ac, 
-		molestie et nisl. Mauris consectetur mi a dolor porttitor, eu tempus velit 
-		malesuada. Duis iaculis sit amet mi sit amet hendrerit."
+		Aenean blandit eu diam eu dictum."
+	},
+	{
+		name: "JLG-35e", 
+		category: "Manlift",
+		daily_price: "$100.00", 
+		weekly_price: "$300.00", 
+		monthly_price: "$900.00", 
+		spec_url: "https://csapps.jlg.com/OnlineManuals/Manuals/JLG/JLG%20Boom%20Lifts/30_35_40_45E/Service_3120743_10-01-01_ANSI_English.pdf", 
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+		Aenean blandit eu diam eu dictum."
+	},
+	{
+		name: "JLG-450AG", 
+		category: "Manlift",
+		daily_price: "$100.00", 
+		weekly_price: "$300.00", 
+		monthly_price: "$900.00", 
+		spec_url: "http://www.jlg.com/en/equipment/engine-powered-boom-lifts/articulating/450aj", 
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+		Aenean blandit eu diam eu dictum."
 	}
 ]
 
